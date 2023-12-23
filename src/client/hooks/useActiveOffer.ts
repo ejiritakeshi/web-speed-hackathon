37e4ba2f0df58fa240327cca58d786770534cb8e
ejiri <takeshi_ejiri@mail.toyota.co.jp>
2023-12-24 02:00:39 +0900

    modify useActiveOffer

--- src/client/hooks/useActiveOffer.ts
+++ src/client/hooks/useActiveOffer.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import type { LimitedTimeOfferFragmentResponse, ProductFragmentResponse } from '../graphql/fragments';
 import { getActiveOffer } from '../utils/get_active_offer';
@@ -6,21 +6,16 @@ import { getActiveOffer } from '../utils/get_active_offer';
 export function useActiveOffer(product: ProductFragmentResponse | undefined) {
   const [activeOffer, setActiveOffer] = useState<LimitedTimeOfferFragmentResponse | undefined>(undefined);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (!product) {
-        setActiveOffer(undefined);
-        return;
-      }
-
-      const offer = getActiveOffer(product.offers);
+  if (!product) {
+    if (activeOffer !== undefined) {
+      setActiveOffer(undefined);
+    }
+  } else {
+    const offer = getActiveOffer(product.offers)
+    if (offer?.id !== activeOffer?.id) {
       setActiveOffer(offer);
-    }, 0);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [product]);
+    }
+  }
 
   return { activeOffer };
 }
