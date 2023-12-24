f5055c493c3111822f41cd0adc27679a5e06713d
ejiri <takeshi_ejiri@mail.toyota.co.jp>
2023-12-24 23:59:49 +0900

    fix useSlider

--- src/client/components/feature/ProductListSlider/hooks/useSlider.ts
+++ src/client/components/feature/ProductListSlider/hooks/useSlider.ts
@@ -4,14 +4,23 @@ const ITEM_MIN_WIDTH = 250 as const;
 
 export const useSlider = ({ items }: { items: unknown[] }) => {
   const containerElementRef = useRef<HTMLUListElement>(null);
-  const [visibleItemCount, setVisibleItemCount] = useState(1);
+  const [visibleItemCount, _setVisibleItemCount] = useState(1);
   const [_slideIndex, setSlideIndex] = useState(0);
   const slideIndex = Math.min(Math.max(0, _slideIndex), items.length - 1);
 
-  const containerWidth = containerElementRef.current?.getBoundingClientRect().width ?? 0;
   useEffect(() => {
-    setVisibleItemCount(Math.max(Math.floor(containerWidth / ITEM_MIN_WIDTH), 1));
-  }, [containerWidth]);
+    const setVisibleItemCount = () => {
+      const containerWidth = containerElementRef.current?.getBoundingClientRect().width ?? 0;
+      _setVisibleItemCount(Math.max(Math.floor(containerWidth / ITEM_MIN_WIDTH), 1));
+    };
+    setVisibleItemCount();
+
+    window.addEventListener('resize', setVisibleItemCount);
+
+    return () => {
+      window.removeEventListener('resize', setVisibleItemCount);
+    }
+  }, []);
 
   return {
     containerElementRef,
