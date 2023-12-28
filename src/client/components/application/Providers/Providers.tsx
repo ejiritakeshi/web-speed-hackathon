5f90ffff7ffd5a5cfcd0f9faee2c0de2962c94d0
ejiri <takeshi_ejiri@mail.toyota.co.jp>
2023-12-28 21:49:43 +0900

    suspenseをやめた

--- src/client/components/application/Providers/Providers.tsx
+++ src/client/components/application/Providers/Providers.tsx
@@ -1,25 +1,23 @@
-import { ApolloProvider, SuspenseCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 import type { FC, ReactNode } from 'react';
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 const Fallback = lazy(() => import('../../../pages/Fallback'));
-import { apolloClient } from '../../../utils//apollo_client';
+import { apolloClient } from '../../../utils/apollo_client';
 
 type Props = {
   children: ReactNode;
 };
 
-const suspenseCache = new SuspenseCache();
-
 export const Providers: FC<Props> = ({ children }) => (
-  <ApolloProvider client={apolloClient} suspenseCache={suspenseCache}>
+  <ApolloProvider client={apolloClient}>
     <BrowserRouter>
       <RecoilRoot>
         <ErrorBoundary fallbackRender={Fallback}>
-          <Suspense fallback={null}>{children}</Suspense>
+          {children}
         </ErrorBoundary>
       </RecoilRoot>
     </BrowserRouter>
